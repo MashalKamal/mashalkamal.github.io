f285fd07542bab89356a41f053c931be8c6b38ba
Mashal Gharzai <105239072+MashalKamal@users.noreply.github.com>
2026-04-25 09:16:03 -0400

    Create filter.js

---
+++ mehr_projects.html/scripts/filter.js
@@ -0,0 +1,13 @@
+function filterSelection(category) {
+    const items = document.querySelectorAll(".item");
+
+    items.forEach(item => {
+        if (category === "all") {
+            item.style.display = "block";
+        } else if (item.classList.contains(category)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
